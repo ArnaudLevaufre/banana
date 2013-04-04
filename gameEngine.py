6ec28f18fd3b988527c57086e0cd37a6258cb450
ArnaudXXIX <arnaudxxix@gmail.com>
2013-04-05 01:15:31 +0200

    la propretée c'est pas pour les chiens

--- gameEngine.py
+++ gameEngine.py
@@ -14,7 +14,6 @@ class GameEngine(pyglet.window.Window):
         pyglet.gl.glEnable(pyglet.gl.GL_BLEND)
         pyglet.clock.schedule_interval(lambda x:x, 1/1000000.0) # Debridage complet des FPS
         pyglet.clock.schedule_interval(self.physicEngine, 1/100.0)
-        self.state = "cin"
 
         # Input handler        
         self.keysHandler = pyglet.window.key.KeyStateHandler()
@@ -25,7 +24,7 @@ class GameEngine(pyglet.window.Window):
 
         self.mainDrawingBatch = pyglet.graphics.Batch()
         self.fpsText = pyglet.text.Label("", x=4, y=self.height, anchor_y="top", batch=self.mainDrawingBatch)
-        self.state = "playing"
+        self.state = "cin"
         self.game = game.Game()
         self.fpsText = pyglet.text.Label("", x=4, y=self.height, anchor_y="top", batch=self.mainDrawingBatch, color=(0,0,0,255))
         self.background = pyglet.image.create(self.width, self.height, pyglet.image.SolidColorImagePattern((255,255,255,255))) # Background
