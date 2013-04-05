799f872290e1707c4534f904e4d46ba150be3664
Bastien <bastien@bastien-PC.(none)>
2013-04-05 18:08:17 +0200

    Ajout de plusieurs fonctionnalitées pour la bordure, height, width, velY, maxY

--- cinematic.py
+++ cinematic.py
@@ -1,54 +1,85 @@
+# coding=utf-8
 import pyglet
 import gameEngine
 import xml.etree.ElementTree as ET
 import time
+
+
 class Cinematic:
-    def __init__(self):
+    
+    def __init__(self, filename = None):
+        # ---- Variables ---- #
         self.lastOnDrawTime = time.time()
         self.dt = 1
         self.W_HEIGHT = gameEngine.GameEngine.W_HEIGHT
         self.W_WIDTH = gameEngine.GameEngine.W_WIDTH
         self.mainDrawingBatch = pyglet.graphics.Batch()
         self.elements = []
+        # Construction
         self.constructFromFile()
 
     def constructFromFile(self):
+        """
+        Construction d'une cinematique a partir d'un fichier xml
+        """
         fileName = "Cin/Cin1.xml" 
+        # On parse le XML
         tree = ET.parse(fileName)
         root = tree.getroot()
         for child in root:
-            if(child.tag == "borderBot"):
-                self.elements.append(Border("bot"))
-            elif(child.tag == "borderTop"):
-                self.elements.append(Border("top"))
+            if(child.tag == "borderBot"): # Si on veut une bordure bot
+                self.elements.append(Border("bot", **child.attrib))
+            elif(child.tag == "borderTop"): # Bordure top
+                self.elements.append(Border("top", **child.attrib))
                 
-        for elmt in self.elements:
+        for elmt in self.elements: # On ajoute tout dans le batch
             elmt.batch = self.mainDrawingBatch
             
-    def run(self):
+    def run(self): # Fonction pour afficher notre cinematique a l'écran
         self.dt = time.time() - self.lastOnDrawTime
         self.lastOnDrawTime = time.time()
         for elmt in self.elements:
-            elmt.animate(self.dt)
+            elmt.animate(self.dt) # On anime tous les elements
         self.mainDrawingBatch.draw()
 
+
+#########################################################   
+#                     Bordure                           #
+#########################################################
 class Border(pyglet.sprite.Sprite):
-    def __init__(self, pos):  
-        print pos
+    def __init__(self, pos, height = None, width = None, velY = 80, maxY = None):
+        # ---- Constantes ---- #
         self.W_HEIGHT = gameEngine.GameEngine.W_HEIGHT
         self.W_WIDTH = gameEngine.GameEngine.W_WIDTH
-        super(Border, self).__init__(pyglet.image.create(self.W_WIDTH, self.W_HEIGHT/4, pyglet.image.SolidColorImagePattern((0,0,0,255))))
+        # ---- Taille ---- #
+        if(height == None):
+            height = self.W_HEIGHT/4
+        if(width == None):
+            width = self.W_WIDTH
+        # ---- Vitesse ---- #
+        self.velY = int(velY)
+        # ---- Image ---- #
+        super(Border, self).__init__(pyglet.image.create(width, height, pyglet.image.SolidColorImagePattern((0,0,0,255))))
+        # ---- Position ---- #
         self.pos = pos
         if(pos == "bot"):
             self.y = -self.height
+            if(maxY == None):
+                self.maxY = 0
+            else:
+                self.maxY = int(maxY)
         elif(pos == "top"):
+            if(maxY == None):
+                self.maxY = self.W_HEIGHT - self.height +2
+            else:
+                self.maxY = int(maxY)
             self.y = self.W_HEIGHT
-            
+        print self.maxY
+
     def animate(self,dt):
         if(self.pos == "bot"):
-            if(self.y < self.W_HEIGHT/4 - self.height):
-                self.y += 20 * dt
-                print self.y
+            if(int(self.y) < self.maxY):
+                self.y += self.velY * dt
         elif(self.pos == "top"):
-            if(self.y > self.W_HEIGHT - self.height +2):
-                self.y -= 20 * dt
+            if(int(self.y) > self.maxY):
+                self.y -= self.velY * dt
