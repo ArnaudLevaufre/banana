e99df3087a4aa6946827ef5d65dd10a9148bd394
ArnaudXXIX <arnaudxxix@gmail.com>
2013-04-05 19:16:47 +0200

    rectification du systeme de map

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
-import time, math
+#-*- encoding: utf-8 -*-
+
 import pyglet, pyglet.window.key as key
 import gameEngine, entity
 
@@ -10,12 +11,12 @@ class Game:
 #        self.entity
 #        self.level
 #        self.camera
-        self.mainBatch = pyglet.graphics.Batch()
+        self.uiBatch = pyglet.graphics.Batch()
         image = pyglet.image.load('sprites/vis.png')
-        print image
-        self.cursor = pyglet.sprite.Sprite(image, gameEngine.GameEngine.W_WIDTH/2, gameEngine.GameEngine.W_HEIGHT/2, batch = self.mainBatch)
+        
+        self.map = Map()
+        self.cursor = pyglet.sprite.Sprite(image, gameEngine.GameEngine.W_WIDTH/2, gameEngine.GameEngine.W_HEIGHT/2, batch = self.uiBatch)
         self.player = entity.Player(200,200)
-        self.camera = Camera()
         
     def simulate(self, dt, keysHandler):
         if keysHandler[key.Z]:
@@ -28,33 +29,53 @@ class Game:
         elif keysHandler[key.D]:
             self.player.move(10, 0)
         
-        # on update la camera
-        self.camera.moveTo(gameEngine.GameEngine.W_WIDTH/2 - self.player.x, gameEngine.GameEngine.W_HEIGHT/2 - self.player.y )
-
+        self.map.setRelativePos( -self.player.x, -self.player.y)
     
     def on_mouse_motion(self,x,y,dx,dy):
-        print x,y
-        print self.cursor.x
-        self.cursor.x = x
-        self.cursor.y = y
+        self.cursor.x = x - self.cursor.width / 2
+        self.cursor.y = y - self.cursor.height / 2
+        
+    def render(self):        
+        xCenter = gameEngine.GameEngine.W_WIDTH / 2
+        yCenter = gameEngine.GameEngine.W_HEIGHT / 2
+        
+        pyglet.gl.glBegin(pyglet.gl.GL_QUADS)
+        
+        pyglet.gl.glVertex2i( xCenter, yCenter )
+        pyglet.gl.glVertex2i( xCenter, yCenter + 16 )
+        pyglet.gl.glVertex2i( xCenter + 16, yCenter + 16 )
+        pyglet.gl.glVertex2i( xCenter + 16, yCenter )
+        pyglet.gl.glEnd()
+        
+        self.map.render()
+        self.uiBatch.draw()
+        
+class Map:
+    """
+    La carte est sibolisée par une matrice de chiffres
+    représentant les éléments graphiques tels que les murs.
+    Elle est divisé en case de 16 pixels par 16 pixels
+    """
+    def __init__(self):
+        self.batch = pyglet.graphics.Batch()
+        self.map = []
+        self.xRelative = 0
+        self.yRelative = 0
+        self.tileSize = 48
+        
+    def setRelativePos(self, x, y):
+        self.xRelative = x
+        self.yRelative = y
+        
+    def load(self, filename):
+        pass
+    
     def render(self):
         pyglet.gl.glBegin(pyglet.gl.GL_QUADS)
-        pyglet.gl.glVertex2i( self.player.x,self.player.y )
-        pyglet.gl.glVertex2i( self.player.x,self.player.y + 10 )
-        pyglet.gl.glVertex2i( self.player.x + 10,self.player.y + 10 )
-        pyglet.gl.glVertex2i( self.player.x + 10,self.player.y )
+        pyglet.gl.glVertex2i( self.xRelative,self.yRelative )
+        pyglet.gl.glVertex2i( self.xRelative ,self.yRelative + self.tileSize )
+        pyglet.gl.glVertex2i( self.xRelative + self.tileSize, self.yRelative + self.tileSize )
+        pyglet.gl.glVertex2i( self.xRelative + self.tileSize,self.yRelative )
         pyglet.gl.glEnd()
-        self.mainBatch.draw()
         
-class Camera:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
     
-    def moveTo(self, x, y):
-        self.x = int(x)
-        self.y = int(y)       
-        pyglet.gl.glViewport(self.x,self.y, gameEngine.GameEngine.W_WIDTH , gameEngine.GameEngine.W_HEIGHT)
-        
-    def reset(self):
-        self.move(0, 0)
