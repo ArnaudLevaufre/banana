2c447479305053dd5094273c1fb0ec39e7d9dddc
ArnaudXXIX <arnaudxxix@gmail.com>
2013-04-09 22:54:24 +0200

    Ajout du model blarg pour le joueur. Cependant l'image 48*48 ne rentre pas correctement sur la surface de 48*48 ... openGL diminue sa taille NEED RECHERCHE.

--- entity.py
+++ entity.py
@@ -20,8 +20,11 @@ class Player(Entity):
         self.hp = 100
         self.width = 48
         self.height = 48
+        
+        self.texture = pyglet.image.load("sprites/blarg.png").get_texture()
+
     def move(self, x,y, gameMap ,dt):
-        if not gameMap.colide(self.x - self.width/2 + x * dt * 50, self.y - self.height/2 + y * dt * 50, self.width, self.height):
+        if not gameMap.colide( self.x - (self.width - 20)/2 + x * dt * 50, self.y - self.height/2 + y * dt * 50, self.width - 20, self.height):
             self.x += x * dt * 50
             self.y += y * dt * 50
             
@@ -29,12 +32,30 @@ class Player(Entity):
         w = gameEngine.GameEngine.W_WIDTH
         h = gameEngine.GameEngine.W_HEIGHT
         
+        # voir la vraie taille!
+#        pyglet.gl.glBegin(pyglet.gl.GL_QUADS)
+#        pyglet.gl.glVertex2i( w/2 - self.width/2, h/2 - self.width/2 )
+#        pyglet.gl.glVertex2i( w/2 + self.width/2, h/2 - self.width/2 )
+#        pyglet.gl.glVertex2i( w/2 + self.width/2, h/2 + self.width/2 )
+#        pyglet.gl.glVertex2i( w/2 - self.width/2, h/2 + self.width/2 )
+#        pyglet.gl.glEnd()
+        
+        pyglet.gl.glEnable(pyglet.gl.GL_TEXTURE_2D)
+        pyglet.gl.glBindTexture(self.texture.target, self.texture.id)
+        
         pyglet.gl.glBegin(pyglet.gl.GL_QUADS)
+        pyglet.gl.glTexCoord2i(0,0)
         pyglet.gl.glVertex2i( w/2 - self.width/2, h/2 - self.width/2 )
+        pyglet.gl.glTexCoord2i(1,0)
         pyglet.gl.glVertex2i( w/2 + self.width/2, h/2 - self.width/2 )
+        pyglet.gl.glTexCoord2i(1,1)
         pyglet.gl.glVertex2i( w/2 + self.width/2, h/2 + self.width/2 )
+        pyglet.gl.glTexCoord2i(0,1)
         pyglet.gl.glVertex2i( w/2 - self.width/2, h/2 + self.width/2 )
         pyglet.gl.glEnd()
+        
+                
+        pyglet.gl.glDisable(pyglet.gl.GL_TEXTURE_2D)
         
 class Npc(object):
     def __init__(self,x,y):
