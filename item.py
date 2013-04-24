d63d5d777d9e46ff593c5006d317830337764327
ArnaudXXIX <arnaudxxix@gmail.com>
2013-04-24 21:06:45 +0200

    rien.

--- item.py
+++ item.py
@@ -1,5 +1,6 @@
 import pyglet
 import math
+import time
 class Item(object):
     def __init__(self, x, y):
         self.x = x
@@ -16,16 +17,17 @@ class Shield(Item):
         # - Constantes -
         self.type = "shield"
         self.value = 50
-        self.SIZE = 48
+        self.SIZE = 16
         
         # - Divers -
         self.tick = 0
+        
     def render(self):
         self.tick +=1
         
         pyglet.gl.glBegin(pyglet.gl.GL_QUADS)
-        pyglet.gl.glVertex2d(self.x - self.SIZE/2 ,self.y - self.SIZE/2 + math.cos(0.2*self.tick))
-        pyglet.gl.glVertex2d(self.x - self.SIZE/2 + self.SIZE ,self.y - self.SIZE/2+ math.cos(0.2*self.tick))
-        pyglet.gl.glVertex2d(self.x - self.SIZE/2 + self.SIZE, self.y - self.SIZE/2 + self.SIZE+ math.cos(0.2*self.tick))
-        pyglet.gl.glVertex2d(self.x - self.SIZE/2 ,self.y - self.SIZE/2 + self.SIZE+ math.cos(0.2*self.tick))
+        pyglet.gl.glVertex2d(self.x - self.SIZE/2 ,self.y - self.SIZE/2 + 5 * math.cos(5 * time.time()))
+        pyglet.gl.glVertex2d(self.x - self.SIZE/2 + self.SIZE ,self.y - self.SIZE/2+ 5 * math.cos(5 * time.time()))
+        pyglet.gl.glVertex2d(self.x - self.SIZE/2 + self.SIZE, self.y - self.SIZE/2 + self.SIZE+ 5 * math.cos(5 * time.time()))
+        pyglet.gl.glVertex2d(self.x - self.SIZE/2 ,self.y - self.SIZE/2 + self.SIZE+ 5 * math.cos(5 * time.time()))
         pyglet.gl.glEnd()
