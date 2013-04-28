1e25cb9acbed673e3736fd83c840b5bb046e9693
ArnaudXXIX <arnaudxxix@gmail.com>
2013-04-28 16:45:54 +0200

    Mise a jour des animations.

--- entity.py
+++ entity.py
@@ -203,7 +203,7 @@ class Player(Entity):
         
     def render(self):
         # - On récupere l'orientation du joueur -
-        if time.time() - self.lastFrameChange > 0.1:
+        if time.time() - self.lastFrameChange > 0.2:
             self.lastFrameChange = time.time()
             
             if self.frame == 3:
