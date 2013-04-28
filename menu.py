69e9d8f635419c1bf5583eb938f43a5c535c9fda
Eijebong <eijebong@myopera.com>
2013-04-28 20:36:17 +0000

    Reduction taille

--- menu.py
+++ menu.py
@@ -13,11 +13,11 @@ class MainMenu():
        
         # Texte
 
-        self.playText = pyglet.text.Label("Jouer",font_size=30, batch=self._batch,anchor_x ="center", anchor_y="top",x=gameEngine.GameEngine.W_WIDTH/2, y=self.principalMenuText.y -80, color=(255,255,255,255))
+        self.playText = pyglet.text.Label("Jouer",font_size=20, batch=self._batch,anchor_x ="center", anchor_y="top",x=gameEngine.GameEngine.W_WIDTH/2, y=self.principalMenuText.y -80, color=(255,255,255,255))
        
-        self.loadText = pyglet.text.Label("Charger une partie",font_size=30, batch=self._batch,anchor_x ="center", anchor_y="top",x=gameEngine.GameEngine.W_WIDTH/2, y=self.playText.y -50, color=(255,255,255,255))
+        self.loadText = pyglet.text.Label("Charger une partie",font_size=20, batch=self._batch,anchor_x ="center", anchor_y="top",x=gameEngine.GameEngine.W_WIDTH/2, y=self.playText.y -50, color=(255,255,255,255))
        
-        self.quitText = pyglet.text.Label("Quitter",font_size=30, batch=self._batch,anchor_x ="center", anchor_y="top",x=gameEngine.GameEngine.W_WIDTH/2, y=self.loadText.y -50, color=(255,255,255,255))      
+        self.quitText = pyglet.text.Label("Quitter",font_size=20, batch=self._batch,anchor_x ="center", anchor_y="top",x=gameEngine.GameEngine.W_WIDTH/2, y=self.loadText.y -50, color=(255,255,255,255))      
         
         self.returnState = "menu"
         
@@ -25,35 +25,35 @@ class MainMenu():
         # - Jouer -
         pyglet.gl.glBegin(pyglet.gl.GL_QUADS)
         pyglet.gl.glColor3ub(33,33,33)
-        pyglet.gl.glVertex2i(self.playText.x-200, self.playText.y-47)  
+        pyglet.gl.glVertex2i(self.playText.x-180, self.playText.y-32)  
         pyglet.gl.glColor3ub(33,33,33)     
-        pyglet.gl.glVertex2i(self.playText.x+200,self.playText.y-47)
+        pyglet.gl.glVertex2i(self.playText.x+180,self.playText.y-32)
         pyglet.gl.glColor3ub(77,77,77)
-        pyglet.gl.glVertex2i(self.playText.x+200,self.playText.y -6)
+        pyglet.gl.glVertex2i(self.playText.x+180,self.playText.y - 4 )
         pyglet.gl.glColor3ub(77,77,77)
-        pyglet.gl.glVertex2i(self.playText.x-200,self.playText.y-6)
+        pyglet.gl.glVertex2i(self.playText.x-180,self.playText.y - 4)
         pyglet.gl.glEnd()
         # - Charger -
         pyglet.gl.glBegin(pyglet.gl.GL_QUADS)
         pyglet.gl.glColor3ub(33,33,33)
-        pyglet.gl.glVertex2i(self.loadText.x-200, self.loadText.y-47)  
+        pyglet.gl.glVertex2i(self.loadText.x-180, self.loadText.y-32)  
         pyglet.gl.glColor3ub(33,33,33)     
-        pyglet.gl.glVertex2i(self.loadText.x+200,self.loadText.y-47)
+        pyglet.gl.glVertex2i(self.loadText.x+180,self.loadText.y-32)
         pyglet.gl.glColor3ub(77,77,77)
-        pyglet.gl.glVertex2i(self.loadText.x+200,self.loadText.y -6)
+        pyglet.gl.glVertex2i(self.loadText.x+180,self.loadText.y -4)
         pyglet.gl.glColor3ub(77,77,77)
-        pyglet.gl.glVertex2i(self.loadText.x-200,self.loadText.y-6)
+        pyglet.gl.glVertex2i(self.loadText.x-180,self.loadText.y-4)
         pyglet.gl.glEnd()
         #-Quitter
         pyglet.gl.glBegin(pyglet.gl.GL_QUADS)
         pyglet.gl.glColor3ub(33,33,33)
-        pyglet.gl.glVertex2i(self.quitText.x-200, self.quitText.y-47)  
+        pyglet.gl.glVertex2i(self.quitText.x-180, self.quitText.y-32)  
         pyglet.gl.glColor3ub(33,33,33)     
-        pyglet.gl.glVertex2i(self.quitText.x+200,self.quitText.y-47)
+        pyglet.gl.glVertex2i(self.quitText.x+180,self.quitText.y-32)
         pyglet.gl.glColor3ub(77,77,77)
-        pyglet.gl.glVertex2i(self.quitText.x+200,self.quitText.y -6)
+        pyglet.gl.glVertex2i(self.quitText.x+180,self.quitText.y -4)
         pyglet.gl.glColor3ub(77,77,77)
-        pyglet.gl.glVertex2i(self.quitText.x-200,self.quitText.y-6)
+        pyglet.gl.glVertex2i(self.quitText.x-180,self.quitText.y-4)
         pyglet.gl.glEnd()
         self._batch.draw()
 
@@ -61,11 +61,11 @@ class MainMenu():
         
     def on_mouse_press(self,x, y, button, modifiers):
         if(button == pyglet.window.mouse.LEFT):
-            if x > self.playText.x - 200 and x < self.playText.x + 200 and y > self.playText.y - 45 and y < self.playText.y - 8:
+            if x > self.playText.x - 180 and x < self.playText.x + 180 and y > self.playText.y - 45 and y < self.playText.y - 8:
                 self.returnState = "playing"
-            if x > self.loadText.x - 185 and x < self.loadText.x + 185 and y > self.loadText.y - 45 and y < self.loadText.y - 8:
+            if x > self.loadText.x - 180 and x < self.loadText.x + 180 and y > self.loadText.y - 45 and y < self.loadText.y - 8:
                 self.returnState = "playing"
-            elif x > self.quitText.x - 65 and x < self.quitText.x + 65 and y > self.quitText.y - 45 and y < self.quitText.y - 8:
+            elif x > self.quitText.x - 180 and x < self.quitText.x + 180 and y > self.quitText.y - 45 and y < self.quitText.y - 8:
                 self.returnState = "quit"
     
     def on_mouse_motion(self,x, y, dx, dy):
